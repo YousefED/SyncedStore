0eaa565c80ddd9d659404cb02ac28d8454382f7b
Yousef <yousefdardiry@gmail.com>
2021-11-12 23:20:19 +0100

    Svelte (#25)
    * svelte example
    
    * move svelte to package
    
    * svelte example readme

---
+++ examples/todo-svelte/README.md
@@ -0,0 +1,17 @@
+# Svelte Todo MVC example
+
+This example demonstrates how you can use SyncedStore to build a collaborative version of the [Todo MVC](http://todomvc.com) app.
+
+The code is based on the [official Svelte Todo MVC example](https://github.com/sveltejs/svelte-todomvc). Minimum changes were needed to make the App work with SyncedStore.
+
+<!-- # Live demo
+
+- [Open live demo](https://uie1c.csb.app/) (Of course, open multiple times to test multiplayer)
+- [Open source on Codesandbox](https://codesandbox.io/s/todo-vue-uie1c) -->
+
+# Running
+
+    npm install
+    npm run dev
+
+🔥 Make sure to open multiple browsers to see the app syncing automatically.
---
+++ packages/svelte/src/index.ts
@@ -0,0 +1,41 @@
+import { readable } from "svelte/store";
+import { Observer, reactive } from "@reactivedata/reactive";
+
+/**
+ * Svelte helper to enable smart Reactive rerendering of your component.
+ *
+ * The usage of the return value is automatically tracked.
+ * The component where you use this store is then rerendered automatically if any of the
+ * used values change.
+ *
+ * e.g.:
+ *
+ * // Store setup:
+ * const globalStore = SyncedStore({ people: [] });
+ * globalStore.items.push({ name: "Alice" });
+ * globalStore.items.push({ name: "Bob" });
+ *
+ * // In your component:
+ * const store = svelteSyncedStore(globalStore);
+ * <div>{$store.items[1].name}</div>
+ *
+ * Now, your component only rerenders when Bob's name changes
+ *  (or if the second element of the array changes)
+ */
+export function svelteSyncedStore<T>(syncedObject: T) {
+  let set: any;
+  const observer = new Observer(() => {
+    if (set) {
+      set(store);
+    }
+  });
+  const store = reactive(syncedObject, observer);
+
+  return readable(store, (newSet) => {
+    set = newSet;
+
+    return () => {
+      set = undefined;
+    };
+  });
+}
