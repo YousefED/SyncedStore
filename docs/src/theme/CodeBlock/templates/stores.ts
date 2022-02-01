be4e83b9f214aa6a47107bdcdb696930d6615c1c
Yousef El-Dardiry <yousefdardiry@gmail.com>
2022-02-01 16:44:24 +0100

    remove fragment from svelte store

--- docs/src/theme/CodeBlock/templates/stores.ts
+++ docs/src/theme/CodeBlock/templates/stores.ts
@@ -52,7 +52,7 @@ import { WebrtcProvider } from "y-webrtc";
 import { svelteSyncedStore } from "@syncedstore/svelte";
 
 // Create your SyncedStore store
-export const store = syncedStore({ todos: [], fragment: "xml" });
+export const store = syncedStore({ todos: [] });
 export const svelteStore = svelteSyncedStore(store);
 
 // Create a document that syncs automatically using Y-WebRTC
