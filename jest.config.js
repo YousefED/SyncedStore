8dd9500651821756445658fa75da8467f8e4e5da
bigmistqke <vandijckv@gmail.com>
2023-02-01 18:41:49 +0100

    add `solid-js`,`solid-js/web` and `solid-js/store` to `moduleNameMapper`

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,8 @@ module.exports = {
   moduleNameMapper: {
     "@syncedstore/core": "<rootDir>/packages/core/src",
     "@syncedstore/yjs-reactive-bindings": "<rootDir>/packages/yjs-reactive-bindings/src",
+    "solid-js/web": "<rootDir>/node_modules/solid-js/web/dist/web.cjs",
+    "solid-js/store": "<rootDir>/node_modules/solid-js/store/dist/store.cjs",
+    "solid-js": "<rootDir>/node_modules/solid-js/dist/solid.cjs",
   },
 };
